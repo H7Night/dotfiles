95c69dca5926610b9fddfd147d75a5b02f6f8e48
hezhongxian <jhonhe7night@outlook.com>
2024-08-31 17:38:46 +0800

    up Rime

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,6 +6,23 @@ return {
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
+    -- "H" to show or hide the hide files
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+            -- 'thumbs.db',
+          },
+          never_show = {".git"},
+        },
+      }
+    },
     config = function()
         vim.keymap.set('n', '<C-n>',':Neotree filesystem reveal left<CR>', {})
     end
