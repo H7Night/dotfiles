db583fabecdbfc71ad2b957320182743659b6966
John He <charaznablu@tutanota.com>
2024-09-10 11:08:16 +0800

    up - nvim

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,29 +1,22 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
+    dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim"},
     -- "H" to show or hide the hide files
     opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          show_hidden_count = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-          hide_by_name = {
-            ".git",
-            ".DS_Store",
-            -- 'thumbs.db',
-          },
-          never_show = {".git"},
-        },
-      }
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = true,
+                hide_by_name = {".git", ".DS_Store" -- 'thumbs.db',
+                },
+                never_show = {".git"}
+            }
+        }
     },
     config = function()
-        vim.keymap.set('n', '<C-n>',':Neotree filesystem reveal left<CR>', {})
+        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
     end
 }
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,4 +1,26 @@
-vim.g.mapleader = " "vim.cmd("set expandtab")
+vim.g.mapleader = " "
+vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+-- 在 INSERT, COMMAND, TERMINAL 模式下，按下 <Shift+Space> 开启或关闭输入法
+keymap({'c', 'i', 't'}, '<S-Space>', function()
+    vim.g.neovide_input_ime = not vim.g.neovide_input_ime
+end, {
+    silent = true
+})
+
+local glob = vim.g
+if glob.neovide then
+    glob.neovide_input_ime = false
+end
+
+local is_win = vim.fn.has("win32") == 1
+if is_win then
+    -- Windows特定设置
+    vim.opt.shell = "powershell"
+    vim.opt.guifont = "JetBrainsMono NF:h11"
+else
+    -- macOS或Linux设置
+    vim.opt.guifont = "JetBrainsMono Nerd Font:h11"
+end
