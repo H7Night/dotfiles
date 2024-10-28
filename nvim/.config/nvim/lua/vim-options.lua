c4c379d4d787f82e194a53d9f57a4cfa7f379a2a
John He <charaznablu@tutanota.com>
2024-10-28 11:53:33 +0800

    up - Win Rime

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -8,25 +8,26 @@ local ime_input = vim.api.nvim_create_augroup("ime_input", {
     clear = true
 })
 -- 模式切换后关闭输入法
-vim.api.nvim_create_autocmd({"ModeChanged"}, {
-    group = ime_input,
-    pattern = "*",
-    callback = function()
-        vim.g.neovide_input_ime = false
-    end
-})
+-- vim.api.nvim_create_autocmd({"ModeChanged"}, {
+--     group = ime_input,
+--     pattern = "*",
+--     callback = function()
+--         vim.g.neovide_input_ime = false
+--     end
+-- })
 
-local glob = vim.g
-if glob.neovide then
-    glob.neovide_input_ime = false
-end
+-- local glob = vim.g
+-- if glob.neovide then
+--     glob.neovide_input_ime = false
+-- end
 
+-- 系统配置
 local is_win = vim.fn.has("win32") == 1
 if is_win then
     -- Windows特定设置
-    vim.opt.shell = "powershell"
-    vim.opt.guifont = "FiraMono Nerd Font:h11"
+    -- vim.opt.shell = "powershell"
+    vim.opt.guifont = "FiraCode Nerd Font Mono:h11"
 else
     -- macOS或Linux设置
-    vim.opt.guifont = "FiraMono Nerd Font:h11"
+    vim.opt.guifont = "FiraCode Nerd Font Mono:h11"
 end
